18a9a538ee42d58223dedaf99a6409bf194153b0
James <35617601+jliby@users.noreply.github.com>
2021-10-18 17:49:52 -0400

    Add files via upload

---
+++ player.js
@@ -0,0 +1,11 @@
+Amplitude.init({
+  songs: [
+    {
+      name: "That’s What I Like",
+      artist: "Bruno Mars",
+      album: "24K Magic",
+      url: "./music/Thats What I Like - Bruno Mars.mp3",
+      cover_art_url: "./music/bruno.jpg",
+    },
+  ],
+});
